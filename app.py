2dc4c1e73d1fd9fd2e9df23bb9279a3e9d1c6452
ab2n <bgriot@franceactive-alsace.org>
2025-10-30 15:46:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Éditeur noir-sur-noir — touche Échap", layout="wide")
-st.title("Éditeur B&B (Esc)")
+st.title("")
 
 html = r"""
 <!doctype html>
